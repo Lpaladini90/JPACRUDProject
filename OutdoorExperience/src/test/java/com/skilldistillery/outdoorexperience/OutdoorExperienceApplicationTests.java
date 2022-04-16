f5d0bd2a8fa4e8d07a23c43a3a58f78f44663493
Lpaladini90 <lpaladini@me.com>
2022-04-15 18:31:31 -0700

    stubb springboot

---
+++ OutdoorExperience/src/test/java/com/skilldistillery/outdoorexperience/OutdoorExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.outdoorexperience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutdoorExperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
